ccf584faef9196fe27e50a15e5df4db600177c81
Anastasia <demerkova16ma521@gmail.com>
2020-06-17 13:10:37 +0300

    hw1

---
+++ hw/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import CatsList from './components/CatsList';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <CatsList />
+    );
+  }
+}
+
+export default App;
